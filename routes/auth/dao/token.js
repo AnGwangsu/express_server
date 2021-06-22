c56bf49f0379362ffa02e7d92496d252b77421d3
안광수 <jhk7716@gmail.com>
2021-06-22 14:50:49 +0900

    토큰 체크,토큰갱신 완료

--- routes/auth/dao/token.js
+++ routes/auth/dao/token.js
@@ -1,8 +1,59 @@
+//DB
+var { User } = require("../../../models");
+//jwt
+const jwt = require("jsonwebtoken");
+//secret
+const secret = require("../../../config/jwt").KEY.secret;
 
-exports.tokenCheck = async (req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+exports.tokenCheck = (req, res) => {
+  try {
+    var accessToken = req.headers.authorization || req.body.authorization;
+    jwt.verify(accessToken, secret, (err, _) => {
+      if (err) {
+        console.log("토큰 만료" + err);
+        res.status(403).json({ resultCode: -30, data: null });
+      } else {
+        console.log("토큰 체크 성공");
+        res.status(200).json({ resultCode: 1, data: null });
+      }
+    });
+  } catch (error) {
+    console.log("토큰체크 실패" + error);
+    res.status(400).json({ resultCode: -1, data: null });
+  }
+};
+
+exports.renewToken = (req, res) => {
+  try {
+    var accessToken = req.body.accessToken;
+    var refreshToken = req.body.refreshToken;
+    var accountId = 0;
+    jwt.verify(accessToken,secret,(err,user)=>{
+        if(err){
+            console.log('accessToken만료'+err)
+            jwt.verify(refreshToken,secret,(err,user)=>{
+                if(err){
+                    console.log('refreshToken만료'+err)
+                    res.status(403).json({"resultCode":-30, "data":null})
+                }else{
+                    accountId=user.accountId
+                    accessToken=jwt.sign({accountId},secret,{expiresIn:'24h'})
+                    refreshToken=jwt.sign({accountId},secret,{expiresIn:'30d'})
+                    console.log('token갱신 성공')
+                    res.status(200).json({"resultCode":1, "data":{accountId,accessToken,refreshToken}})
+                }
+            })
+        }else{
+            console.log('accessToken확인 완료')
+            accountId=user.accountId
+            accessToken=jwt.sign({accountId},secret,{expiresIn:'24h'})
+            refreshToken=jwt.sign({accountId},secret,{expiresIn:'30d'})
+            console.log('token갱신 성공')
+            res.status(200).json({"resultCode":1, "data":{accountId,accessToken,refreshToken}})
+        }
+    })
+  } catch (error) {
+      console.log('token갱신 실패'+error)
+      res.status(400).json({"resultCode":-1, "data":null})
+  }
+};
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -5,19 +5,24 @@ const passport = require('../../middleware/passport')
 const token = require('./dao/token')
 const login = require('./dao/login')
 const join = require('./dao/join')
+const auth = require('./dao/auth')
 
 router.post('/auth/tokenCheck',token.tokenCheck)
 //카카오 로그인
 router.get('/kakao',passport.kakao.authenticate('kakao')); //서버 테스트를 위함, front에서 동의페이지로 넘어가서 필요x
 router.get('/kakao/callback',passport.kakao.authenticate('kakao'),login.socialLogin)
 //네이버 로그인
-router.get('/naver',passport.naver.authenticate('naver',{ scope: ['profile'] }))
+router.get('/naver',passport.naver.authenticate('naver',{ scope: ['profile'] }))//서버 테스트를 위함, front에서 동의페이지로 넘어가서 필요x
 router.get('/naver/callback',passport.naver.authenticate('naver'),login.socialLogin)
-
 //일반 로그인
 router.post('/login',login.login)
-
 //회원가입
 router.post('/join',join.join)
+//토큰체크
+router.post('/tokenCheck',token.tokenCheck)
+//토큰갱신
+router.post('/renewToken',token.renewToken)
+//아이디 중복체크
+router.post('/checkId',auth.checkId)
 
 module.exports = router
