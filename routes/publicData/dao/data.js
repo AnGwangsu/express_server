c7ff7fba1d235a0daf0a457c5a4aafd2682db8a9
안광수 <jhk7716@gmail.com>
2021-06-25 16:57:00 +0900

    공공데이터 api추가

--- routes/publicData/dao/data.js
+++ routes/publicData/dao/data.js
@@ -2,72 +2,155 @@ const axios = require("axios");
 
 exports.locationList = async (req, res) => {
   try {
-    var ServiceKey=process.env.PUBLIC_DATA_SERVICEKEY//Deconding된 인증키를 사용해야한다
-    var pageNo = req.body.page
-    var numOfRows = req.body.limit
-    var areaCode = req.body.areaCode
-    var arrange = req.body.arrange
-    var MobileApp = "tour"
-    var MobileOS = "ETC"
-    var items = []
-    var response = await axios.get('http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList',{
-          params:{
-              ServiceKey,
-              pageNo,
-              numOfRows,
-              MobileApp,
-              MobileOS,
-              arrange,
-              areaCode,
-          }
-      })
-    items = response.data.response.body.items
-    console.log('공공데이터 지역기반 리스트 성공')
+    var ServiceKey = process.env.PUBLIC_DATA_SERVICEKEY; //Deconding된 인증키를 사용해야한다
+    var pageNo = req.body.page;
+    var numOfRows = req.body.limit;
+    var areaCode = req.body.areaCode;
+    var contentTypeId = req.body.contentTypeId;
+    var cat1 = req.body.cat1;
+    var cat2 = req.body.cat2;
+    var cat3 = req.body.cat3;
+    var arrange = req.body.arrange;
+    var MobileApp = "tour";
+    var MobileOS = "ETC";
+    var items = [];
+    var response = await axios.get(
+      "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList",
+      {
+        params: {
+          ServiceKey,
+          pageNo,
+          numOfRows,
+          MobileApp,
+          MobileOS,
+          arrange,
+          areaCode,
+          contentTypeId,
+          cat1,
+          cat2,
+          cat3,
+        },
+      }
+    );
+    items = response.data.response.body.items.item;
+    console.log("공공데이터 지역기반 리스트 성공");
+    res.status(200).json({ resultCode: 1, data: { items: items } });
+  } catch (error) {
+    console.log("공공데이터 지역기반 리스트 실패" + error);
+    res.status(400).json({ resultCode: -1, data: null });
+  }
+};
+
+exports.enterLocation = async (req, res) => {
+  try {
+    var ServiceKey = process.env.PUBLIC_DATA_SERVICEKEY; //Deconding된 인증키를 사용해야한다
+    var pageNo = req.body.page;
+    var numOfRows = req.body.limit;
+    var areaCode = req.body.areaCode;
+    var contentTypeId = req.body.contentTypeId;
+    var cat1 = req.body.cat1;
+    var cat2 = req.body.cat2;
+    var arrange = req.body.arrange;
+    var MobileApp = "tour";
+    var MobileOS = "ETC";
+    var items = [];
+    var response = await axios.get(
+      "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList",
+      {
+        params: {
+          ServiceKey,
+          pageNo,
+          numOfRows,
+          MobileApp,
+          MobileOS,
+          arrange,
+          areaCode,
+          contentTypeId,
+          cat1,
+          cat2,
+        },
+      }
+    );
+    items = response.data.response.body.items.item;
+    console.log('즐길거리 리스트 완료')
     res.status(200).json({"resultCode":1, "data":{"items":items}})
   } catch (error) {
-    console.log('공공데이터 지역기반 리스트 실패'+error)
+    console.log('즐길거리 리스트 실패'+error)
     res.status(400).json({"resultCode":-1, "data":null})
+  }
+};
+
+exports.locationCode = async (req, res) => {
+  try {
+    var ServiceKey = process.env.PUBLIC_DATA_SERVICEKEY;
+    var MobileApp = "tour";
+    var MobileOS = "ETC";
+    var areaCode = req.body.areaCode;
+    var items = [];
+    var response = await axios.get(
+      "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode",
+      {
+        params: {
+          ServiceKey,
+          MobileApp,
+          MobileOS,
+          areaCode,
+        },
+      }
+    );
+    items = response.data.response.body.items.item;
+    console.log("공공데이터 지역코드 리스트 성공");
+    res.status(200).json({ resultCode: 1, data: { items: items } });
+  } catch (error) {
+    console.log("공공데이터 지역코드 리스트 실패");
+    res.status(400).json({ resultCode: -1, data: null });
   }
 };
 
 exports.read = async (req, res) => {
   try {
-    var ServiceKey=process.env.PUBLIC_DATA_SERVICEKEY
-    var MobileApp = "tour"
-    var MobileOS = "ETC"
-    var contentId = req.body.contentId
-    var contentTypeId = req.body.contentTypeId
-    var mapx = req.body.mapx
-    var mapy =req.body.mapy
-    var image = await axios.get('http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailImage',{
-        params:{
-            ServiceKey,
-            MobileApp,
-            MobileOS,
-            contentId,
-            contentTypeId
-        }
-    })
-    var path=''
-    image = image.data.response.body.items.item
-    for(i in image){
-        path=image[i].originimgurl
-        image[i]=path
+    var ServiceKey = process.env.PUBLIC_DATA_SERVICEKEY;
+    var MobileApp = "tour";
+    var MobileOS = "ETC";
+    var contentId = req.body.contentId;
+    var contentTypeId = req.body.contentTypeId;
+    var mapx = req.body.mapx;
+    var mapy = req.body.mapy;
+    var image = await axios.get(
+      "http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailImage",
+      {
+        params: {
+          ServiceKey,
+          MobileApp,
+          MobileOS,
+          contentId,
+          contentTypeId,
+        },
+      }
+    );
+    var path = "";
+    image = image.data.response.body.items.item;
+    for (i in image) {
+      path = image[i].originimgurl;
+      image[i] = path;
     }
-    var intro = await axios.get('http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailIntro',{
-        params:{
-            ServiceKey,
-            MobileApp,
-            MobileOS,
-            contentId,
-            contentTypeId
-        }
-    })
-    intro = intro.data.response.body
-    console.log('지역기반 상세 보기 성공')
-    res.status(200).json({"resultCode":1, "data":{image,intro,mapx,mapy}})
+    var intro = await axios.get(
+      "http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailIntro",
+      {
+        params: {
+          ServiceKey,
+          MobileApp,
+          MobileOS,
+          contentId,
+          contentTypeId,
+        },
+      }
+    );
+    intro = intro.data.response.body;
+    console.log("지역기반 상세 보기 성공");
+    res.status(200).json({ resultCode: 1, data: { image, intro, mapx, mapy } });
   } catch (error) {
-    console.log('지역기반 상세 보기 실패'+error)
-    res.status(400).json({"resultCode":-1, "data":null})
+    console.log("지역기반 상세 보기 실패" + error);
+    res.status(400).json({ resultCode: -1, data: null });
   }
 };
--- routes/publicData/index.js
+++ routes/publicData/index.js
@@ -3,9 +3,16 @@ const router = require('express').Router()
 //로직
 var data = require('./dao/data')
 
+
+//지역코드 리스트(http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode)
+router.post('/locationCode',data.locationCode)
+
 //지역기반 리스트(http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList)
 router.post('/locationList',data.locationList)
 
+
+//문화공연/스포츠 리스트
+router.post('/locationEnter',data.enterLocation)
 //카테고리 기반
 
 //숙박업소
